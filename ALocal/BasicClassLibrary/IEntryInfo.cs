2223e7a8140f49eeb55b6ec1c8ad782b0067c99d
Rimirin-U <T202410486994650@eduxiji.net>
2025-04-10 16:10:10 +0800

    条目信息接口IEntryInfo实现

---
+++ ALocal/BasicClassLibrary/IEntryInfo.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BasicClassLibrary
+{
+    public interface IEntryInfo
+    {
+        public int Id { get; set; }         // 内键
+        public int EntryId { get; set; }    // 外键（唯一）
+        public Entry Entry { get; set; }    // 导航属性
+    }
+}
