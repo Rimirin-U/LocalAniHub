dd9ce081af3871ae87c7e2cc1808ad64934617b7
Rimirin-U <T202410486994650@eduxiji.net>
2025-04-12 16:12:15 +0800

    Material类完成

---
+++ ALocal/BasicClassLibrary/Material.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BasicClassLibrary
+{
+    public class Material : IEntryNavigation
+    {
+        public Material(int? entryId, Entry? entry, string name, string kind, string path)
+        {
+            EntryId = entryId;
+            Entry = entry;
+            this.name = name;
+            this.kind = kind;
+            this.path = path;
+        }
+
+        public int? EntryId { get; set; }
+        public Entry? Entry { get; set; }
+
+        public string name { get; set; }
+        public string kind { get; set; }
+        public string path { get; set; }
+    }
+}
