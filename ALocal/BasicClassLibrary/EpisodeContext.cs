cf59f9ec988b509c92ac344afccd9e566466a309
Rimirin-U <T202410486994650@eduxiji.net>
2025-04-13 17:18:19 +0800

    context change

--- ALocal/BasicClassLibrary/EpisodeContext.cs
+++ ALocal/BasicClassLibrary/EpisodeContext.cs
@@ -7,14 +7,8 @@ using System.Threading.Tasks;
 
 namespace BasicClassLibrary
 {
-    public class EpisodeContext : DbContext
+    public partial class AppContext : DbContext
     {
         public DbSet<Episode> Episodes { get; set; }
-
-        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
-        {
-            var path = Path.Combine(Directory.GetCurrentDirectory(), "data/episodes.db");
-            optionsBuilder.UseSqlite($"Data source={path}");
-        }
     }
 }
