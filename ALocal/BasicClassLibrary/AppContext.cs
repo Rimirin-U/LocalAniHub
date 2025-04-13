d39f49ee1cdd017929c1a11a5a9b86815cbba729
Rimirin-U <T202410486994650@eduxiji.net>
2025-04-13 17:23:48 +0800

    AppDbContext 初始化

---
+++ ALocal/BasicClassLibrary/AppContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BasicClassLibrary
+{
+    public partial class AppDbContext : DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            var path = Path.Combine(Directory.GetCurrentDirectory(), "data/data.db");
+            optionsBuilder.UseSqlite($"Data source={path}");
+        }
+    }
+}
