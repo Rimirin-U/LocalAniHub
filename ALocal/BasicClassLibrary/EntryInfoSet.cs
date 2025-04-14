3b2d31935ee8d5d48889ea9ccca890787b53dc50
sweet <15485723+wdswt@user.noreply.gitee.com>
2025-04-14 18:37:05 +0800

    2

--- ALocal/BasicClassLibrary/EntryInfoSet.cs
+++ ALocal/BasicClassLibrary/EntryInfoSet.cs
@@ -6,7 +6,22 @@ using System.Threading.Tasks;
 
 namespace BasicClassLibrary
 {
-    internal class EntryInfoSet
+    public class EntryInfoSet
     {
+        public string TranslatedName { get; }//译名
+        public string OriginalName { get; }//原名
+        public DateTime ReleaseDate { get; }//首播/上映日期
+        public string Category { get; }//类别
+        //系列键值对，存储从网络上得到的其他信息
+        public IReadOnlyDictionary<string, string> Metadata { get; }
+        public EntryInfoSet(string translatedName, string originalName,
+            DateTime releaseDate, string category, IReadOnlyDictionary<string, string> metadata)
+        {
+            TranslatedName = translatedName;
+            OriginalName = originalName;
+            ReleaseDate = releaseDate;
+            Category = category;
+            Metadata = metadata;
+        }
     }
 }
