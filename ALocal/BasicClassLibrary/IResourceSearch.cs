876887a3d8a9b8df47576fc9efdc168f2b98278e
Rimirin-U <T202410486994650@eduxiji.net>
2025-04-14 17:34:56 +0800

    IResourceSearch初始化

---
+++ ALocal/BasicClassLibrary/IResourceSearch.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ServiceModel.Syndication;
+
+namespace BasicClassLibrary
+{
+    public interface IResourceSearch
+    {
+        public List<SyndicationFeed> search(string kw);
+    }
+}
