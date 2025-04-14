7ce405982a5c2cbf96588b422787c228590dc015
Rimirin-U <T202410486994650@eduxiji.net>
2025-04-14 17:38:27 +0800

    IResourceSearch完成

--- ALocal/BasicClassLibrary/IResourceSearch.cs
+++ ALocal/BasicClassLibrary/IResourceSearch.cs
@@ -9,6 +9,6 @@ namespace BasicClassLibrary
 {
     public interface IResourceSearch
     {
-        public List<SyndicationFeed> search(string kw);
+        public List<SyndicationFeed> search(string keyWord);
     }
 }
