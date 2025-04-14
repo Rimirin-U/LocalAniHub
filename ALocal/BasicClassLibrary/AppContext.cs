40d0c000d55713941c4825b2b3f392fb14da883c
sweet <15485723+wdswt@user.noreply.gitee.com>
2025-04-14 15:16:30 +0800

    MaterialService,Resource,ResourceService

---
+++ ALocal/BasicClassLibrary/AppContext.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BasicClassLibrary
+{
+    public partial class AppDbContext : DbContext
+    {
+        public DbSet<Entry> Entries { get; set; }
+        public DbSet<Material> Materials { get; set; }
+        public DbSet<EntryMetadata> EntryMetadata { get; set; }
+        public DbSet<Resource> Resources { get; set; }
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            var path = Path.Combine(Directory.GetCurrentDirectory(), "data/data.db");
+            optionsBuilder.UseSqlite($"Data source={path}");
+        }
+    }
+}
---
+++ ALocal/BasicClassLibrary/Resource.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+// 资源对象
+namespace BasicClassLibrary
+{
+    public class Resource:IEpisodeNavigation
+    {
+        public int ResourceId { get; set; } // 主键 资源ID
+        //IEpisodeNavigation
+        public int? EpisodeId { get; set; }    // 外键 对应集数ID
+        public Episode? Episode { get; set; }    // 导航属性  
+        public DateTime ImportData { get; set; } // 导入时间
+        public bool HasSubtitle { get; set; } // 是否有外挂字幕
+        public string? ResourcePath { get; set; } //路径
+        //
+        public Resource(int? episodeId,Episode? episode,
+            DateTime importData,bool hasSubtitle,string path)
+        {
+            EpisodeId = episodeId;
+            Episode = episode;
+            ImportData = importData;
+            HasSubtitle = hasSubtitle;
+            ResourcePath = path;
+        }
+    }
+}
