ee2ba203395d67c2b837284137965a44a8e05867
Rimirin-U <T202410486994650@eduxiji.net>
2025-04-14 18:29:53 +0800

    Note完成

---
+++ ALocal/BasicClassLibrary/Note.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BasicClassLibrary
+{
+    public class Note : IEntriesNavigation, IEpisodesNavigation
+    {
+        public Note()
+        {
+            this.Content = "";
+            this.EntriesId = new List<int>();
+            this.Entries = new List<Entry>();
+            this.EpisodesId = new List<int>();
+            this.Episodes = new List<Episode>();
+        }
+
+        public int Id { get; set; }
+
+        public ICollection<int> EntriesId { get; set; }
+        public ICollection<Entry> Entries { get; set; }
+        public ICollection<int> EpisodesId { get; set; }
+        public ICollection<Episode> Episodes { get; set; }
+
+        public string Content { get; set; }
+    }
+}
