37418250c2fc346ab72e648dff2291a3c31d81a2
sweet <15485723+wdswt@user.noreply.gitee.com>
2025-04-16 21:29:04 +0800

    更新

--- ALocal/BasicClassLibrary/EntryRating.cs
+++ ALocal/BasicClassLibrary/EntryRating.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Microsoft.EntityFrameworkCore;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,11 +7,15 @@ using System.Threading.Tasks;
 
 namespace BasicClassLibrary
 {
-    public class EntryRating: IEntryNavigation
+    public class EntryRating: IEntryNavigation, IEntityWithId
     {
         public int Id { get; set; } // 主键
         public int? EntryId { get; set; } // 外键
         public Entry? Entry { get; set; } // 导航属性
         public double? Score { get; set; } // 评分
+    }
+    public partial class AppDbContext : DbContext
+    {
+        public DbSet<EntryRating> EntryRatings { get; set; }
     }
 }
