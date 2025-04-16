3ee0e308be4de0fe6e9aab4edd02285e08f45100
Rimirin-U <T202410486994650@eduxiji.net>
2025-04-16 19:36:47 +0800

    新增EntryRatingManager

---
+++ ALocal/BasicClassLibrary/EntryRatingManager.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BasicClassLibrary
+{
+    public class EntryRatingManager : Manager<EntryRating>
+    {
+        public EntryRatingManager() : base(new AppDbContext()) { }
+
+        public static Func<EntryRating, bool> ByEntryId(int entryId) => (o => o.EntryId == entryId);
+        public static readonly Func<EntryRating, bool> All = (o => true);
+    }
+}
+}
