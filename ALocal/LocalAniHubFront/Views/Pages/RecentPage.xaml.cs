5a09f9286a5d79c1d83fc783455f1a61e3a09f4e
Rimirin-U <T202410486994650@eduxiji.net>
2025-04-28 18:29:27 +0800

    RecentPage VIEW初步完成

---
+++ ALocal/LocalAniHubFront/Views/Pages/RecentPage.xaml.cs
@@ -0,0 +1,30 @@
+﻿using LocalAniHubFront.ViewModels.Pages;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using Wpf.Ui.Abstractions.Controls;
+
+namespace LocalAniHubFront.Views.Pages
+{
+    public partial class RecentPage : INavigableView<RecentPageViewModel>
+    {
+        public RecentPage(RecentPageViewModel viewModel)
+        {
+            ViewModel = viewModel;
+            DataContext = this;
+            InitializeComponent();
+        }
+
+        public RecentPageViewModel ViewModel { get; }
+    }
+}
