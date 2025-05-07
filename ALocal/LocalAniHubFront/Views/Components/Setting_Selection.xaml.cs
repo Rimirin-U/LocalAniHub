95e40873f63335a4cc5eec0e67938885eb1665d3
Rimirin-U <T202410486994650@eduxiji.net>
2025-05-07 17:21:52 +0800

    Setting_Selection Setting_TextBox VIEW完成

---
+++ ALocal/LocalAniHubFront/Views/Components/Setting_Selection.xaml.cs
@@ -0,0 +1,28 @@
+﻿using LocalAniHubFront.Models;
+using LocalAniHubFront.ViewModels.Components;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace LocalAniHubFront.Views.Components
+{
+    public partial class Setting_Textbox : UserControl
+    {
+        public Setting_Textbox(SelectionSettingEntry settingEntry)
+        {
+            DataContext = new Setting_SelectionViewModel(settingEntry);// 该ViewModel构造函数传参
+            InitializeComponent();
+        }
+    }
+}
---
+++ ALocal/LocalAniHubFront/Views/Components/Setting_TextBox.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Windows.Controls;
+
+namespace LocalAniHubFront.Views.Components
+{
+    public partial class Setting_Textbox : UserControl
+    {
+        // 可以先写Setting_Selection
+        public Setting_Textbox(TextboxSettingEntry settingEntry)// 这个类要定义在Models文件夹中
+        {
+            DataContext = new Setting_TextBoxViewModel(settingEntry);// 自行创建对应文件:)
+            InitializeComponent();
+        }
+    }
+}
