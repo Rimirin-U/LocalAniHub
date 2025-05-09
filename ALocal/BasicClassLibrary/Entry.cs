c600473f3849dd3ec32923c719d3c56a83fdfbf0
zjl20050627 <3093870235@qq.com>
2025-05-09 10:40:37 +0800

    修改Enter.cs

--- ALocal/BasicClassLibrary/Entry.cs
+++ ALocal/BasicClassLibrary/Entry.cs
@@ -30,12 +30,12 @@ namespace BasicClassLibrary
         public State State { get; set; }         // 观看状态
         public string MaterialFolder { get; set; } // 素材子文件夹名称
         //配合素材服务的属性
-        public int KeyVisualId { get; set; } // 主封面图ID
+        public string KeyVisualId { get; set; } // 主封面图ID
 
         //设置项
         public bool HasUpdateTime {  get; set; }//是否有更新时自动获取资源
         public bool AutoClearResources {  get; set; }//是否自动清除资源
-        public Entry(int id,string translatedName,string originalName,DateTime releaseDate,DateTime collectionDate,string category,int episodeCount, State state,string materialFolder, int keyVisualId, bool hasUpdateTime,bool autoClearResources)    
+        public Entry(int id,string translatedName,string originalName,DateTime releaseDate,DateTime collectionDate,string category,int episodeCount, State state,string materialFolder, string keyVisualId, bool hasUpdateTime,bool autoClearResources)    
         {
             Id = id;
             TranslatedName = translatedName;
