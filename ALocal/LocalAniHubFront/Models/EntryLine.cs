49bf4ff2dfada840dc26f161bdc6f8d9982873d4
CXT-good <1723730831@qq.com>
2025-05-11 10:16:06 +0800

    修改Collection_SimpleEntryTimeViewModel

---
+++ ALocal/LocalAniHubFront/Models/EntryLine.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LocalAniHubFront.Models
+{
+    public class EntryLine
+    {
+        public string LineText { get; set; } // 示例: "原名 (译名) - 上映时间"
+        public int Id { get; set; } // 条目 ID
+    }
+}
