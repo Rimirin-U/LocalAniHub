4c0e3075cb2c8702e4f48915d9a886da919e1ddc
zjl20050627 <3093870235@qq.com>
2025-05-12 21:12:29 +0800

    增加EntryWindow_MaterialManageViewModel.cs,只提供出MaterialFolderPath

---
+++ ALocal/LocalAniHubFront/ViewModels/Pages/EntryWindow_MaterialManageViewModel.cs
@@ -0,0 +1,31 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
+using System.Collections.ObjectModel;
+using System.IO;
+using System.Linq;
+using System.Windows.Media.Imaging;
+using LocalAniHubFront.Helpers;
+using BasicClassLibrary;
+
+namespace LocalAniHubFront.ViewModels.Pages
+{
+    public partial class EntryWindow_MaterialManageViewModel
+    {
+        public int EntryId { get; }
+
+        public EntryWindow_MaterialManageViewModel(int entryId)
+        {
+            EntryId = entryId;
+        }
+
+        public string MaterialFolderPath
+        {
+            get
+            {
+                // 假设全局设置服务已实现
+                string parentFolder = GlobalSettingsService.Instance.GetValue("defaultMaterialParentFolderPath");
+                return Path.Combine(parentFolder, EntryId.ToString());
+            }
+        }
+    }
+}
