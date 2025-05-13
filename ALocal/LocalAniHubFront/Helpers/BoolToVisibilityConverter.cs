4d22fffa02b882b24f89ca5594fa53959d0690be
Rimirin-U <T202410486994650@eduxiji.net>
2025-05-13 12:22:18 +0800

    控件与helper增加并注册

---
+++ ALocal/LocalAniHubFront/Helpers/BoolToVisibilityConverter.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Data;
+
+namespace UiDesktopApp1.Helpers
+{
+    public class BoolToVisibilityConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            bool invert = parameter?.ToString() == "false";
+            bool flag = System.Convert.ToBoolean(value);
+            return (invert ? !flag : flag) ? Visibility.Visible : Visibility.Collapsed;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture) =>
+            (value is Visibility v && v == Visibility.Visible);
+    }
+
+}
