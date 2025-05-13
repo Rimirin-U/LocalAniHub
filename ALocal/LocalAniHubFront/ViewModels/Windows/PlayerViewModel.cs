0a56b25283d9e242542e244241aff2ffc5943411
Rimirin-U <T202410486994650@eduxiji.net>
2025-05-13 16:22:54 +0800

    PlayerWindow初步完成

---
+++ ALocal/LocalAniHubFront/ViewModels/Windows/PlayerViewModel.cs
@@ -0,0 +1,92 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
+using LibVLCSharp.Shared;
+using System;
+
+namespace LocalAniHubFront.ViewModels.Windows
+{
+    public partial class PlayerViewModel : ObservableObject, IDisposable
+    {
+        private readonly LibVLC _libVLC;
+        private readonly MediaPlayer _mediaPlayer;
+        private Media? _currentMedia;
+
+        public MediaPlayer MediaPlayer => _mediaPlayer;
+
+        [ObservableProperty]
+        private string mediaPath = @"D:\Download\[ANi] mono女孩 - 05 [1080P][Baha][WEB-DL][AAC AVC][CHT].mp4";
+
+        public PlayerViewModel()
+        {
+            Core.Initialize();
+            _libVLC = new LibVLC();
+            _mediaPlayer = new MediaPlayer(_libVLC);
+
+            _mediaPlayer.Playing += (_, _) => NotifyAllCommands();
+            _mediaPlayer.Paused += (_, _) => NotifyAllCommands();
+            _mediaPlayer.Stopped += (_, _) => NotifyAllCommands();
+        }
+
+        [RelayCommand(CanExecute = nameof(CanLoad))]
+        private void Load()
+        {
+            _currentMedia = new Media(_libVLC, MediaPath, FromType.FromPath);
+            _mediaPlayer.Media = _currentMedia;
+            // _mediaPlayer.Play(_currentMedia);
+            NotifyAllCommands();
+        }
+        private bool CanLoad() => !string.IsNullOrWhiteSpace(MediaPath);
+
+        [RelayCommand(CanExecute = nameof(CanPlay))]
+        private void Play()
+        {
+            _mediaPlayer.Play();
+            NotifyAllCommands();
+        }
+        private bool CanPlay() => !_mediaPlayer.IsPlaying;
+
+        [RelayCommand(CanExecute = nameof(CanPause))]
+        private void Pause()
+        {
+            _mediaPlayer.Pause();
+            NotifyAllCommands();
+        }
+        private bool CanPause() => _mediaPlayer.CanPause;
+
+        [RelayCommand]
+        private void Stop()
+        {
+            _mediaPlayer.Stop();
+            NotifyAllCommands();
+        }
+
+        private void NotifyAllCommands()
+        {
+            if (Application.Current.Dispatcher.CheckAccess())
+            {
+                LoadCommand.NotifyCanExecuteChanged();
+                PlayCommand.NotifyCanExecuteChanged();
+                PauseCommand.NotifyCanExecuteChanged();
+                StopCommand.NotifyCanExecuteChanged();
+            }
+            else
+            {
+                Application.Current.Dispatcher.Invoke(() =>
+                {
+                    LoadCommand.NotifyCanExecuteChanged();
+                    PlayCommand.NotifyCanExecuteChanged();
+                    PauseCommand.NotifyCanExecuteChanged();
+                    StopCommand.NotifyCanExecuteChanged();
+                });
+            }
+        }
+
+
+        public void Dispose()
+        {
+            _currentMedia?.Dispose();
+            _mediaPlayer.Dispose();
+            _libVLC.Dispose();
+        }
+    }
+}
---
+++ ALocal/LocalAniHubFront/Views/Windows/PlayerWindow.xaml.cs
@@ -0,0 +1,30 @@
+﻿using System.Windows;
+using LocalAniHubFront.ViewModels.Windows;
+
+namespace LocalAniHubFront.Views.Windows
+{
+    public partial class PlayerWindow 
+    {
+        public PlayerWindow()
+        {
+            InitializeComponent();
+        }
+
+        public PlayerWindow(string mediaUrl) : this()
+        {
+            ViewModel.MediaPath = mediaUrl;
+        }
+
+        // private PlayerViewModel ViewModel => DataContext as PlayerViewModel;
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            ViewModel.LoadCommand.Execute(null);
+        }
+
+        private void Window_Closing(object sender, System.ComponentModel.CancelEventArgs e)
+        {
+            ViewModel.Dispose();
+        }
+    }
+}
