633a4862304be8b930341f2c1e88d8d877f0097d
Rimirin-U <T202410486994650@eduxiji.net>
2025-05-20 14:35:30 +0800

    新增EntryInfoCard相关代码

---
+++ ALocal/LocalAniHubFront/ViewModels/Components/EntryInfoCardViewModel.cs
@@ -0,0 +1,53 @@
+﻿using LibVLCSharp.Shared;
+using LocalAniHubFront.Helpers;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Runtime.InteropServices.JavaScript.JSType;
+using System.Windows.Media;
+using System.Drawing;
+using BasicClassLibrary;
+
+namespace LocalAniHubFront.ViewModels.Components
+{
+    public partial class EntryInfoCardViewModel: ObservableObject
+    {
+        [ObservableProperty]
+        private ObservableCollection<KeyValuePair<string, string>> metadataDict = new();
+
+        [ObservableProperty]
+        [NotifyPropertyChangedFor(nameof(KvImageSource))]
+        private Image kvImage;
+        public ImageSource KvImageSource => ImageHelper.ToImageSource(KvImage); // 真正用于绑定
+
+        [ObservableProperty]
+        private string title;
+
+        [ObservableProperty]
+        private string subtitle;
+
+        [ObservableProperty]
+        private string date;
+
+        [ObservableProperty]
+        private string category;
+
+        public EntryInfoCardViewModel(EntryInfoSet entryInfoSet)
+        {
+            // debug（请替换为实际实现，从EntryInfoSet中读取）
+            MetadataDict.Add(new("企划", "BUSHIROAD"));
+            MetadataDict.Add(new("导演", "柿本广大"));
+            MetadataDict.Add(new("编剧", "绫奈由仁子"));
+            MetadataDict.Add(new("音乐", "藤田淳平 藤间仁"));
+            MetadataDict.Add(new("CV", "羊宫妃那 立石凛 青木阳菜 小日向美香 林鼓子"));
+            KvImage = Image.FromFile(@"D:\reFSC\ConsoleApp1\UiDesktopApp1\Views\Windows\mygo.jpg");
+            Title = "BanG Dream! It's MyGO!!!!!";
+            Subtitle = "迷途之子!!!!!";
+            Date = "2023.6.29";
+            Category = "原创动画";
+        }
+    }
+}
---
+++ ALocal/LocalAniHubFront/Views/Components/EntryInfoCard.xaml.cs
@@ -0,0 +1,37 @@
+﻿using BasicClassLibrary;
+using LocalAniHubFront.ViewModels.Components;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace LocalAniHubFront.Views.Components
+{
+    public partial class EntryInfoCard : UserControl
+    {
+        public EntryInfoCard()
+        {
+            DataContext = new EntryInfoCardViewModel(EntryInfoSet);
+            InitializeComponent();
+        }
+
+        
+        public EntryInfoSet EntryInfoSet { get; set; }
+
+        public static readonly DependencyProperty EntryInfoSetProperty =
+            DependencyProperty.Register(
+                nameof(EntryInfoSet),
+                typeof(EntryInfoSet),
+                typeof(EntryInfoCard));
+    }
+}
