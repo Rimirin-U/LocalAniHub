cd05ffa6a517a94fd2fb22e87765679f0d4b01c6
Rimirin-U <T202410486994650@eduxiji.net>
2025-05-22 00:44:40 +0800

    DownloadViewModel基本完成

--- ALocal/LocalAniHubFront/ViewModels/Pages/DownloadViewModel.cs
+++ ALocal/LocalAniHubFront/ViewModels/Pages/DownloadViewModel.cs
@@ -1,12 +1,31 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using BasicClassLibrary;
+using System.Collections.ObjectModel;
 
 namespace LocalAniHubFront.ViewModels.Pages
 {
-    public class DownloadViewModel
+    public partial class DownloadViewModel: ObservableObject
     {
+        private System.Timers.Timer _statusUpdateTimer;
+        
+        [ObservableProperty]
+        private ObservableCollection<MagnetDownloadManager> downloadManagers= new ();
+
+        public DownloadViewModel()
+        {
+            // 从ResourceDownloadService获取所有下载任务列表，每秒获取一次（**待完成**）
+            // 初始化定时器，每秒更新一次
+            _statusUpdateTimer = new(1000);
+            _statusUpdateTimer.Elapsed += (sender, e) => UpdateDownloadStatuses();// 每秒更新一次下载状态
+            _statusUpdateTimer.Start();
+        }
+
+        // 定时更新下载状态
+        private void UpdateDownloadStatuses()
+        {
+            foreach (var manager in DownloadManagers)
+            {
+                manager.UpdateDownloadStatus();
+            }
+        }
     }
 }
