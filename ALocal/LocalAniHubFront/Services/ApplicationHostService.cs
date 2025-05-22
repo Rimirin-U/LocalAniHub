c3dc93b7d1c030a1780482f5dedda432a2bce2e1
Rimirin-U <T202410486994650@eduxiji.net>
2025-05-22 14:02:53 +0800

    初始化InitializeWindow

--- ALocal/LocalAniHubFront/Services/ApplicationHostService.cs
+++ ALocal/LocalAniHubFront/Services/ApplicationHostService.cs
@@ -48,8 +48,22 @@ namespace LocalAniHubFront.Services
                 _navigationWindow = (
                     _serviceProvider.GetService(typeof(INavigationWindow)) as INavigationWindow
                 )!;
+
+                // 初始化状态检测与窗口打开
+                // 检查是否是首次运行
+                bool isFirstRun = true;// ...
+                if (isFirstRun)
+                {
+                    // 弹出初始化设置窗口
+                    var initializeWindow = new InitializeWindow();
+                    initializeWindow.ShowDialog();  // ShowDialog:模态窗口，关闭后才能继续
+                    // 设置完毕后，更新标志
+                    // ...
+                }
+
+                // 打开MainWindow
                 _navigationWindow!.ShowWindow();
-                // 默认导航定义处
+                // 默认导航定义处 //
                 _navigationWindow.Navigate(typeof(Views.Pages.DashboardPage));
             }
 
