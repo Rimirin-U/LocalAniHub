fc07ce3e0f3d4ad9f2148a1a0176646ec230b4f1
zjl20050627 <3093870235@qq.com>
2025-05-23 22:03:09 +0800

    AddEntryWindow：增强健壮性：若保存的过程中报错导致保存失败 要撤销已完成的所有操作,并且把Save()函数修改为返回bool，若出错则返回false

--- ALocal/BasicClassLibrary/EpisodeManager.cs
+++ ALocal/BasicClassLibrary/EpisodeManager.cs
@@ -10,6 +10,7 @@ namespace BasicClassLibrary
     public class EpisodeManager : Manager<Episode>
     {
         public EpisodeManager() : base(new AppDbContext()) { }
+        public EpisodeManager(AppDbContext context) : base(context) { }
 
         public readonly static Func<int, Func<Episode, bool>> ByEntryId = (entryId =>
                                                                           (ep => ep.EntryId == entryId));
