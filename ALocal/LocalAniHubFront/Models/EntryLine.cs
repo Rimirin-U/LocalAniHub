73f00835e9474420cc2836a57f81bf319a0a5fd7
Rimirin-U <T202410486994650@eduxiji.net>
2025-05-23 19:01:00 +0800

    SympleEntryTImeList新增右键删除

--- ALocal/LocalAniHubFront/Models/EntryLine.cs
+++ ALocal/LocalAniHubFront/Models/EntryLine.cs
@@ -1,4 +1,5 @@
-﻿using LocalAniHubFront.Views.Windows;
+﻿using BasicClassLibrary;
+using LocalAniHubFront.Views.Windows;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -7,7 +8,7 @@ using System.Threading.Tasks;
 
 namespace LocalAniHubFront.Models
 {
-    public partial class EntryLine:ObservableObject
+    public partial class EntryLine : ObservableObject
     {
         public string LineText { get; set; } // 示例: "原名 / 译名（上映时间）"
         public int Id { get; set; } // 条目 ID
@@ -17,6 +18,17 @@ namespace LocalAniHubFront.Models
         {
             var entryWindow = new EntryWindow(Id);
             entryWindow.Show();
+        }
+
+        [RelayCommand]
+        private void DeleteEntry()
+        {
+            if (MessageBox.Show("确定移除该收藏？", "确认删除",
+                MessageBoxButton.YesNo, MessageBoxImage.Warning) == MessageBoxResult.Yes)
+            {
+                EntryManager entryManager = new();
+                entryManager.RemoveById(Id);
+            }
         }
     }
 }
