bfdca5f1d0321940640c1875254b1436f3a6b037
Rimirin-U <T202410486994650@eduxiji.net>
2025-05-24 20:26:28 +0800

    完成

---
+++ ALocal/LocalAniHubFront/Helpers/DateTimeToStringConverter.cs
@@ -0,0 +1,28 @@
+﻿using System.Globalization;
+using System.Windows.Data;
+
+namespace LocalAniHubFront.Helpers
+{
+    public class DateTimeToStringConverter : IValueConverter
+    {
+        // DateTime -> string
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value is DateTime dateTime)
+            {
+                return dateTime.ToString("yyyy.M.d HH:mm", CultureInfo.InvariantCulture);
+            }
+            return string.Empty;
+        }
+
+        // string -> DateTime
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (DateTime.TryParseExact(value?.ToString(), "yyyy.M.d HH:mm", CultureInfo.InvariantCulture, DateTimeStyles.None, out var result))
+            {
+                return result;
+            }
+            return DependencyProperty.UnsetValue;
+        }
+    }
+}
--- ALocal/LocalAniHubFront/ViewModels/Windows/ResourceSearchViewModel.cs
+++ ALocal/LocalAniHubFront/ViewModels/Windows/ResourceSearchViewModel.cs
@@ -1,6 +1,7 @@
 ﻿using BasicClassLibrary;
 using LocalAniHub.ViewModels.Components;
 using System.Collections.ObjectModel;
+using System.Diagnostics;
 
 namespace LocalAniHub.ViewModels.Windows
 {
@@ -9,7 +10,12 @@ namespace LocalAniHub.ViewModels.Windows
         public ObservableCollection<TextBoxItemViewModel> TextBoxes { get; } = new();
 
         [ObservableProperty]
-        private ObservableCollection<ResourceItem> resourceItems;
+        private ObservableCollection<ResourceItem> resourceItems;/* = new() { 
+            new() { Title = "[BeanSub][Kusuriya no Hitorigoto][43][GB][1080P][x264_AAC]", PubDate = new(2025, 5, 24), DownloadUrl = "testUrl" },
+            new() { Title = "[BeanSub][Kusuriya no Hitorigoto][43][GB][720P][x264_AAC]", PubDate = new(2025, 5, 24), DownloadUrl = "testUrl1" },
+            new() { Title = "[BeanSub][Kusuriya no Hitorigoto][43][GB][2160P][x264_AAC]", PubDate = new(2025, 5, 24), DownloadUrl = "testUrl2" },
+            new() { Title = "[BeanSub][Kusuriya no Hitorigoto][42][GB][1080P][x264_AAC]", PubDate = new(2025, 5, 24), DownloadUrl = "testUrl3" }
+        };*/
 
         [ObservableProperty]
         private string windowTitle = "资源搜索: BanG Dream! Ave Mujica 第2集";// 示例
@@ -28,6 +34,14 @@ namespace LocalAniHub.ViewModels.Windows
         {
             // ...
             // ... 以TextBoxes中的所有Text为List<string>，进行资源搜索，以填充resourceItems
+        }
+
+        [RelayCommand]
+        private void AddDownload(string path)
+        {
+            // ...
+            // 添加下载任务，添加成功后弹窗提示
+            Debug.WriteLine($"AddDownloadCommand Called {path}");
         }
 
         private void RemoveTextBox(TextBoxItemViewModel item)
